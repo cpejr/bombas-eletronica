e2a9ea3d31b19d377c454d0a7161eb4be03f9418
Joao Pedro Miranda Marques <jpmm.brasil@hotmail.com>
2020-11-26 15:13:37 -0300

    sensors (not complete)

--- master/master.ino
+++ master/master.ino
@@ -1,23 +1,33 @@
+//Bibliotecas de conexao wifi
 #include <WiFi.h>
 #include <HTTPClient.h>
+
+//Bibliotecas sensor de temperatura
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Wire.h>  //Vem no Arduino já
 
-#define ONE_WIRE_BUS 2 //variavel do pino 2 que esta plugado o Sensor de Temperatura
+//Bibliotecas sensor de Corrente
+#include "EmonLib.h"
+#include <SPI.h>
 
- 
-//Instacia o Objeto oneWire e Seta o pino do Sensor para iniciar as leituras
-OneWire oneWire(ONE_WIRE_BUS);
- 
-// Pass our oneWire reference to Dallas Temperature. 
-//Repassa as referencias do oneWire para o Sensor Dallas (DS18B20)
-DallasTemperature temperature_sensor(&oneWire);
+//Configuracoes Temperatura
+#define ONE_WIRE_BUS 4 //variavel do pino 4 que esta plugado o Sensor de Temperatura
+OneWire oneWire(ONE_WIRE_BUS); //Instacia o Objeto oneWire e Seta o pino do Sensor para iniciar as leituras
+DallasTemperature temperature_sensor(&oneWire); //Repassa as referencias do oneWire para o Sensor Dallas (DS18B20)
 
+//Configuracoes Corrente
+EnergyMonitor emon1;
+int rede = 5; //Tensao da rede eletrica
+int pino_sct = 25; //Pino do sensor SCT
+
+//Configuracoes Rede
+//const char* ssid = "Jota";
+//const char* password = "jota12345";
 const char* ssid = "NET_2GCB0FA0";
 const char* password = "4BCB0FA0";
-
 String systemURL = "http://bombastesteback.herokuapp.com/data/teste";
+
 
 void setup() {
   
@@ -26,6 +36,7 @@ void setup() {
   
   WiFi.begin(ssid, password); 
   temperature_sensor.begin();
+  emon1.current(pino_sct, 60); //Pino, calibracao - Cur Const= Ratio/BurdenR. 2000/33 = 60
   
   while (WiFi.status() != WL_CONNECTED) { //Check for the connection
     delay(1000);
@@ -42,9 +53,15 @@ void loop() {
 
    //Sensor de Temperatura
    temperature_sensor.requestTemperatures();
-   // A temperatura em Celsius para o dispositivo 1 no índice 0 (é possivel ligar varios sensores usando a mesma porta do arduino)
    float temp=temperature_sensor.getTempCByIndex(0);
+   Serial.print("Temperatura : ");
    Serial.println(temp); 
+
+   //Sensor de Corrente
+   double Irms = emon1.calcIrms(1480); //Calcula a corrente
+   Serial.print("Corrente : ");
+   Serial.println(Irms); //Mostra o valor da corrente no serial monitor e display
+   
    
    HTTPClient http;   
   
@@ -76,5 +93,4 @@ void loop() {
  }
   
   delay(300000);  //Send a request every 5 minute
-  //delay(15000);
 }
