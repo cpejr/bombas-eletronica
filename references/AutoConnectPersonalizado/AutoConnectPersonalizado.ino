f4d6821fbb39997b53da74b7c5cb47c37b904bc3
ArthurLima <arthurdeolima@gmail.com>
2021-03-13 00:22:36 -0300

    Pagina personalizada e recebendo post de formulario

---
+++ references/AutoConnectPersonalizado/AutoConnectPersonalizado.ino
@@ -0,0 +1,38 @@
+#include <AutoConnect.h>
+#include <AutoConnectAux.h>
+#include <WiFi.h>
+#include <WebServer.h>
+
+WebServer Server;
+AutoConnect Portal(Server);
+
+AutoConnectAux equipmentIdPage("/equipment_id", "Id do equipamento");
+ACText(header, "Configurações do ID do equipamento");
+ACText(caption, "Aqui você pode configurar o ID deste equipamento");
+ACInput(input1, "", "INPUT");
+ACSubmit(send, "HELLO", "/newIp");
+
+
+void onNewIp() {
+  Server.sendHeader("Location", String("/_ac"), true);
+  Server.send ( 302, "text/plain", "");
+}
+
+void rootPage() {
+  char content[] = "Hello, world";
+  Server.send(200, "text/plain", content);
+}
+
+void setup() {
+  Serial.begin(9600);
+  equipmentIdPage.add({header, caption, input1, send});
+  Portal.join(equipmentIdPage);
+  Server.on("/", rootPage);
+  Server.on("/newIp", onNewIp);
+  if (Portal.begin()) {
+    Serial.println("HTTP server:" + WiFi.localIP().toString());
+  }
+}
+void loop() {
+ Portal.handleClient();
+}
